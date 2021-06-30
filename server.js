af8051928cfaf3ab02fe1a907e2e3774e66b19a1
leJad <stephanscode345@gmail.com>
2021-06-30 05:05:15 -0700

    Fix some problems (after 6 months)

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const yt = require("ytdl-core");
+const path = require("path");
 const app = express();
+
+app.use(express.static(__dirname + "/public"));
 
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
 app.get("/install", (req, res) => {
   var URL = req.query.URL;
-  res.header("Content-Disposition", 'attachment; filename="suckitpatrick.mp4');
+  res.header("Content-Disposition", `attachment; filename="${URL}.mp4`);
   metainf = yt.getBasicInfo(URL);
 
   metainf.then(function (res) {
